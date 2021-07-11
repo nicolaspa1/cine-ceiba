b62d64f3425c8da75f687447be1e70c1b92aa822
nicolaspa1 <nicolaspa@ufps.edu.co>
2021-07-11 09:06:34 -0500

    Cambio datos de prueba

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,4 +1,4 @@
 insert into entrada (numero_funcion, precio, nombre_pelicula, fecha_funcion)
 values (1, 10000, 'el padrino', now());
 insert into afiliado (nombre, tipo_documento, numero_documento, fecha_nacimiento, email, direccion, telefono,fecha_registro)
-values ('Nicolas', 'cc', '10022222', now(), 'asdf@sdf.com', 'dssadsda', '456786',now())
+values ('Nicolas', 'cc', '34566534536', now(), 'asdf456@s55df.com', 'dghdfgfd54', '456786',now())
